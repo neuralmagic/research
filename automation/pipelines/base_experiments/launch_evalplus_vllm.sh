ff564490898cecb02db739e0929abbf746890d46
Alexandre Marques <alexandre@neuralmagic.com>
2024-08-09 18:19:41 +0000

    Fix launch scripts

--- automation/pipelines/base_experiments/launch_evalplus_vllm.sh
+++ automation/pipelines/base_experiments/launch_evalplus_vllm.sh
@@ -1,4 +1,4 @@
-python /network/alexandre/automation/queue_evalplus_vllm.py \
+python evalplus_vllm_base.py \
   --model-id 9310f090b68e40d59226797b9542aa3e \
   --clearml-model \
   --num-gpus 1
--- automation/pipelines/base_experiments/launch_llmcompressor_oneshot_base.sh
+++ automation/pipelines/base_experiments/launch_llmcompressor_oneshot_base.sh
@@ -1,6 +1,4 @@
-
-source ~/environments/clearml/bin/activate
-python /network/alexandre/automation/queue_llmcompressor_oneshot.py \
+python llmcompressor_oneshot_base.py \
   --model-id meta-llama/Meta-Llama-3.1-8B \
   --recipe "/network/alexandre/quantization/recipe_w8a16_rtn.yaml" \
   --tags "llm-compressor" "oneshot"
