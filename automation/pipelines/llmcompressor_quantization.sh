283cd5d337196bbcff7da577562a1c7222f3c568
Alexandre Marques <alexandre@neuralmagic.com>
2024-08-09 17:56:17 +0000

    Remove temporary files

--- automation/pipelines/llmcompressor_quantization.sh
+++
@@ -1,7 +0,0 @@
-
-source ~/environments/clearml/bin/activate
-python /network/alexandre/automation/queue_llmcompressor_oneshot.py \
-  --model-id meta-llama/Meta-Llama-3.1-8B \
-  --recipe "/network/alexandre/quantization/recipe_w8a16_rtn.yaml" \
-  --tags "llm-compressor" "oneshot"
-
