8d174d1759f19bfc7758e4c9a50790391ef81fcf
Alexandre Marques <alexandre@neuralmagic.com>
2024-08-09 19:36:12 +0000

    hf lm-evaluation-task base tasks

---
+++ automation/pipelines/base_experiments/launch_lmevaluation_harness_hf_base.sh
@@ -0,0 +1,2 @@
+python lm_evaluation_harness_hf_base.py \
+  --model-id meta-Llama/Meta-Llama-3.1-8B
---
+++ automation/pipelines/base_experiments/launch_lmevaluation_harness_sparseml_base.sh
@@ -0,0 +1,3 @@
+python lm_evaluation_harness_sparseml_base.py \
+  --model-id 9310f090b68e40d59226797b9542aa3e \
+  --clearml-model
