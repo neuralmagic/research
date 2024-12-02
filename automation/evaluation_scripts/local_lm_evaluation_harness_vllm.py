9226feddd103bb8ea29ac1cc433ffd3039c14907
Alexandre Marques <alexandre@neuralmagic.com>
2024-12-02 04:32:06 +0000

    Add relative path to sys path to allow importing of parsing script

--- automation/evaluation_scripts/local_lm_evaluation_harness_vllm.py
+++ automation/evaluation_scripts/local_lm_evaluation_harness_vllm.py
@@ -25,67 +25,64 @@ parser.add_argument("--num-gpus", type=int, default=None)
 
 args = parser.parse_args()
 
-args = vars(args)
-project_name = args.pop("project_name")
-task_name = args.pop("task_name")
-
 Task.force_store_standalone_script()
 
-task = Task.init(project_name=project_name, task_name=task_name)
+#task = Task.init(project_name=args.project_name, task_name=args.task_name)
 
 from clearml import InputModel
 from glob import glob
 import subprocess
 import os
-from lm_evaluation_harness import push_to_clearml
 import torch
+import importlib.util
+import sys
 
-if args["num_gpus"] is None:
+current_file_path = os.path.abspath(__file__)
+path_to_parsing = os.path.join(current_file_path, "..", "..", "clearml_evaluation_parsing", "lm_evaluation_harness.py")
+sys.path.insert(0, path_to_parsing)
+
+from lm_evaluation_harness import push_to_clearml
+
+if args."num_gpus" is None:
     num_gpus = torch.cuda.device_count()
 else:
-    num_gpus = args["num_gpus"]
+    num_gpus = args."num_gpus"
 
-if args["clearml_model"]:
-    input_model = InputModel(model_id=args["model_id"])
+if args."clearml_model":
+    input_model = InputModel(model_id=args."model_id")
     model_id = input_model.get_local_copy()
     task.connect(input_model)
 else:
-    model_id = args["model_id"]
+    model_id = args."model_id"
 
-max_model_len = args["max_model_len"]
-max_gen_toks = args["max_gen_toks"]
-gpu_memory_utilization = args["gpu_memory_utilization"]
-max_num_seqs = args["max_num_seqs"]
-model_args = f"pretrained={model_id},dtype=auto,max_model_len={max_model_len},max_gen_toks={max_gen_toks},gpu_memory_utilization={gpu_memory_utilization},tensor_parallel_size={num_gpus},max_num_seqs={max_num_seqs}"
-if args["add_bos_token"]:
+model_args = f"pretrained={model_id},dtype=auto,max_model_len={args.max_model_len},max_gen_toks={args.max_gen_toks},gpu_memory_utilization={args.gpu_memory_utilization},tensor_parallel_size={num_gpus},max_num_seqs={args.max_num_seqs}"
+if args.add_bos_token:
     model_args += ",add_bos_token=True"
-if args["trust_remote_code"]:
+if args.trust_remote_code:
     model_args += ",trust_remote_code=True"
-if args["enable_chunked_prefill"]:
-    max_num_batched_tokens = args["max_num_batched_tokens"]
-    model_args += f",enable_chunked_prefill=True,disable_sliding_window=True,max_num_batched_tokens={max_num_batched_tokens}"
-if args["cpu_offload_gb"] is not None:
-    cpu_offload_gb = args["cpu_offload_gb"]
-    model_args += f",cpu_offload_gb={cpu_offload_gb}"
+if args.enable_chunked_prefill:
+    model_args += f",enable_chunked_prefill=True,disable_sliding_window=True,max_num_batched_tokens={args.max_num_batched_tokens}"
+if args.cpu_offload_gb is not None:
+    model_args += f",cpu_offload_gb={args.cpu_offload_gb}"
 
 inputs = [
-    "python3", "-m", "lm_eval", 
+    "lm_eval", 
     "--model", "vllm", 
-    "--tasks", args["benchmark_tasks"], 
+    "--tasks", args.benchmark_tasks, 
     "--model_args", model_args,
     "--write_out", 
     "--show_config", 
     "--output_path", ".",
-    "--batch_size", args["batch_size"],
+    "--batch_size", args.batch_size,
 ]
 
-if args["num_fewshot"] is not None:
-    inputs.extend(["--num_fewshot", str(args["num_fewshot"])])
+if args.num_fewshot is not None:
+    inputs.extend(["--num_fewshot", str(args.num_fewshot)])
 
-if args["apply_chat_template"]:
+if args.apply_chat_template:
     inputs.append("--apply_chat_template")
 
-if args["fewshot_as_multiturn"]:
+if args.fewshot_as_multiturn:
     inputs.append("--fewshot_as_multiturn")
 
 subprocess.run(inputs)
@@ -93,7 +90,7 @@ subprocess.run(inputs)
 model_suffix = os.path.split(model_id)[-1]
 results_dir = glob(f"*{model_suffix}")[-1]
 json_file = glob(os.path.join(results_dir, "results*.json"))[0]
-if args["clearml_model"]:
+if args.clearml_model:
     push_to_clearml(task, json_file, input_model)
 else:
     push_to_clearml(task, json_file)
