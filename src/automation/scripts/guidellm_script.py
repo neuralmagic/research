78839b96bdf018f19c301a4f84fcc981a52c8583
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-07 23:29:41 +0000

    add GuideLLMTask and guidellm_script for benchmark report generation

---
+++ src/automation/scripts/guidellm_script.py
@@ -0,0 +1,90 @@
+import subprocess
+import requests
+import time
+import os
+import sys
+from urllib.parse import urlparse
+from clearml import Task
+import torch
+from automation.utils import resolve_model_id
+from guidellm import generate_benchmark_report
+
+SERVER_LOG_FILE = "vllm_server_log.txt"
+
+
+def start_vllm_server(vllm_args, model_id, target, server_wait_time):
+    executable_path = os.path.dirname(sys.executable)
+    vllm_path = os.path.join(executable_path, "vllm")
+
+    num_gpus = torch.cuda.device_count()
+
+    parsed_target = urlparse(target)
+
+    server_command = [
+        f"{vllm_path}", "serve", 
+        model_id,
+        "--host", parsed_target.hostname, 
+        "--port", str(parsed_target.port),
+        "--tensor-parallel-size", str(num_gpus)
+    ]
+
+    for k, v in vllm_args.items():
+        if v == True:
+            v = "true"
+        server_command.extend([k, str(v)])
+
+    server_log_file = open(SERVER_LOG_FILE, "w")
+    server_process = subprocess.Popen(" ".join(server_command), stdout=server_log_file, stderr=server_log_file, shell=True)
+
+    delay = 5
+    server_initialized = False
+    for _ in range(server_wait_time // delay):
+        try:
+            response = requests.get(target + "/models")
+            if response.status_code == 200:
+                print("Server initialized")
+                server_initialized = True
+                break  # Exit the loop if the request is successful
+        except requests.exceptions.RequestException as e:
+            pass
+
+        time.sleep(delay)
+
+    if server_initialized:
+        return server_process, True
+    else:
+        return server_process, False
+
+def main():
+    task = Task.current_task()
+
+    args = task.get_parameters_as_dict(cast=True)
+    guidellm_args = args["GuideLLM"]
+    environment_args = args.get("environment", {})
+    vllm_args = args.get("vLLM", {})
+
+    # Resolve model_id
+    model_id = resolve_model_id(args["Args"]["model_id"], args["Args"]["clearml_model"], task)
+
+    # Start vLLM server
+    server_process, server_initialized = start_vllm_server(vllm_args, model_id, guidellm_args["target"], args["Args"]["server_wait_time"])
+
+    if not server_initialized:
+        server_process.kill()
+        task.upload_artifact(name="vLLM server log", artifact_object=SERVER_LOG_FILE)
+        raise AssertionError("Server failed to intialize")
+
+    # Parse through environment variables
+    for k, v in environment_args.items():
+        os.environ[k] = str(v)
+  
+    guidellm_args["model"] = model_id
+
+    report = generate_benchmark_report(**guidellm_args)
+    server_process.kill()
+
+    task.upload_artifact(name="guidellm guidance report", artifact_object=report.to_json())
+    task.upload_artifact(name="vLLM server log", artifact_object=SERVER_LOG_FILE)
+
+if __name__ == '__main__':
+    main()
---
+++ src/automation/tasks/guidellm.py
@@ -0,0 +1,86 @@
+from automation.tasks import BaseTask
+from automation.docker import DEFAULT_DOCKER_IMAGE
+from typing import Optional, Sequence
+import os
+
+DEFAULT_SERVER_WAIT_TIME = 600 # 600 seconds = 10 minutes
+
+class GuideLLMTask(BaseTask):
+
+    guidellm_packages = [
+        "vllm",
+        "git+https://github.com/neuralmagic/guidellm.git@http_backend",
+    ]
+
+    def __init__(
+            self,
+            project_name: str,
+            task_name: str,
+            model_id: str,
+            server_wait_time: int=DEFAULT_SERVER_WAIT_TIME,
+            docker_image: str=DEFAULT_DOCKER_IMAGE,
+            packages: Optional[Sequence[str]]=None,
+            clearml_model: bool=False,
+            task_type: str="training",
+            **kwargs,
+    ):
+
+        from automation.scripts.guidellm_script import main
+        from guidellm.main import generate_benchmark_report_cli
+        import click
+
+        if packages is not None:
+            packages = list(set(packages + self.guidellm_packages))
+        else:
+            packages = self.guidellm_packages
+
+        super().__init__(
+            project_name=project_name,
+            task_name=task_name,
+            docker_image=docker_image,
+            packages=packages,
+            task_type=task_type,
+        )
+
+        # Sort vllm and guidellm kwargs
+        guidellm_signature = {param.name: param.default for param in generate_benchmark_report_cli.params if isinstance(param, click.Option)}
+        guidellm_signature["cont_refresh_table"] = guidellm_signature.pop("enable_continuous_refresh")
+
+        vllm_kwargs = {}
+        environment_variables = {}
+        guidellm_kwargs = guidellm_signature
+        for k, v in kwargs.items():
+            if k in guidellm_signature:
+                guidellm_kwargs[k] = v
+            elif k.startswith("GUIDELLM__"):
+                environment_variables[k] = v
+            else:
+                vllm_kwargs[k] = v
+
+        # Store class attributes
+        self.model_id = model_id
+        self.clearml_model = clearml_model
+        self.server_wait_time = server_wait_time
+        self.vllm_kwargs = vllm_kwargs
+        self.guidellm_kwargs = guidellm_kwargs
+        self.environment_variables = environment_variables
+        self.script_path = os.path.join(".", "src", "automation", "scripts", "guidellm_script.py")
+        self.script = main
+
+    def get_arguments(self):
+        args = {
+            "Args": {
+                "model_id": self.model_id,
+                "clearml_model": self.clearml_model,
+                "server_wait_time": self.server_wait_time,
+            },
+            "GuideLLM": self.guidellm_kwargs,
+        }
+
+        if len(self.vllm_kwargs) > 0:
+            args["vLLM"] = self.vllm_kwargs
+
+        if len(self.environment_variables) > 0:
+            args["environment"] = self.environment_variables
+
+        return args
