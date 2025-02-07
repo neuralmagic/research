3f22235c12b86186337da5744bdc57398c728c3d
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-07 20:26:20 +0000

    remove unused upload_results function and clean up lmeval_script.py

--- src/automation/scripts/lmeval_script.py
+++ src/automation/scripts/lmeval_script.py
@@ -1,40 +1,7 @@
-import os
-import glob
-import json
 from clearml import Task
 import torch
 from automation.utils import resolve_model_id, dict_to_argparse
 import lm_eval
-
-def upload_results(model_id, task):
-    model_suffix = os.path.split(model_id)[-1]
-    
-    if len(glob(f"*{model_suffix}")) > 0:
-        results_dir = glob(f"*{model_suffix}")[-1]
-        json_file = glob(os.path.join(results_dir, "results*.json"))[0]
-
-        results = json.load(open(json_file))
-        artifact = {"name": json_file, "object": results}
-        task.upload_artifact(name=json_file, artifact_object=results)
-        
-        scalars = []
-        if "results" in results:
-            for lm_eval_task in results["results"]:
-                if "configs" in results and lm_eval_task in results["configs"] and "num_fewshot" in results["configs"][lm_eval_task]:
-                    num_fewshot = results["configs"][lm_eval_task]["num_fewshot"]
-                else:
-                    num_fewshot = None
-                for metric in results["results"][lm_eval_task]:
-                    value = results["results"][lm_eval_task][metric]
-                    if not isinstance(value, str):
-                        if num_fewshot is None:
-                            name = lm_eval_task + "/" + metric
-                        else:
-                            name = lm_eval_task + "/" + f"{num_fewshot:d}" + "shot/" + metric
-                        task.get_logger().report_single_value(name=name, value=value)
-                        task.get_logger().report_scalar(title=lm_eval_task, series=metric, iteration=num_fewshot, value=value)
-    else:
-        raise Exception("No results found. Evaluation probably failed.")
 
 
 def main():
@@ -94,6 +61,7 @@ def main():
 
     if "groups" in results:
         print(lm_eval.utils.make_table(results, "groups"))
-   
+
+
 if __name__ == '__main__':
     main()
