2ced9dafb4a725b93f555b8678a6839ee1a791d8
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-11 18:28:27 +0000

    add CodeGenerationBenchmarking and OpenLLMTask classes for enhanced benchmarking capabilities

---
+++ src/automation/standards/code_generation_benchmarking.py
@@ -0,0 +1,37 @@
+from automation.tasks import GuideLLMTask, DEFAULT_SERVER_WAIT_TIME
+from automation.docker import DEFAULT_DOCKER_IMAGE
+from typing import Optional, Sequence
+
+
+class CodeGenerationBenchmarking(GuideLLMTask):
+
+    def __init__(
+        self,
+        project_name: str,
+        task_name: str,
+        model_id: str,
+        server_wait_time: int=DEFAULT_SERVER_WAIT_TIME,
+        docker_image: str=DEFAULT_DOCKER_IMAGE,
+        packages: Optional[Sequence[str]]=None,
+        clearml_model: bool=False,
+        **kwargs,
+    ):
+        
+        if "data-type" in kwargs:
+            raise ValueError("data-type should not be specified with CodeGenerationBenchmarking")
+        
+        if "data" in kwargs:
+            raise ValueError("data should not be specified with CodeGenerationBenchmarking")
+        
+        super().__init__(
+            project_name=project_name,
+            task_name=task_name,
+            model_id=model_id,
+            server_wait_time=server_wait_time,
+            docker_image=docker_image,
+            packages=packages,
+            clearml_model=clearml_model,
+            data_type="emulated",
+            data="prompt_tokens=256,generated_tokens=1024",
+            **kwargs,
+        )
