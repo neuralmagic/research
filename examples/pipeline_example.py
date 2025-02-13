c33736410b14fea5c0d6175a8a3d87514202975f
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-13 18:37:55 +0000

    feat: add recursively_parse_parameters option in pipeline_example.py step configuration

--- examples/pipeline_example.py
+++ examples/pipeline_example.py
@@ -49,6 +49,7 @@ pipeline.add_step(
     parents=["pipeline_example_quantization_step1"],
     execution_queue="oneshot-a5000x1",
     parameter_override={"Args/model_id": "${pipeline_example_quantization_step1.models.output.-1.id}"},
+    recursively_parse_parameters=True,
     monitor_metrics=[("Summary", "gsm8k/5shot/exact_match,strict-match")],
 )
 
