1d9259574aabb232a2d5fba84505939147aedbb6
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-23 01:47:08 +0000

    feat: import LeaderboardTask for evaluation standards

--- src/automation/standards/__init__.py
+++ src/automation/standards/__init__.py
@@ -11,6 +11,7 @@ from automation.standards.benchmarking.benchmarking_32k import Benchmarking32kTa
 from automation.standards.benchmarking.benchmarking_64k import Benchmarking64kTask
 from automation.standards.benchmarking.benchmarking_128k import Benchmarking128kTask
 from automation.standards.evaluations.openllm import OpenLLMTask
+from automation.standards.evaluations.leaderboard import LeaderboardTask
 from automation.standards.compression.quantization_w4a16 import QuantizationW4A16Task
 from automation.standards.compression.quantization_w8a8 import QuantizationW8A8Task
 from automation.standards.compression.quantization_fp8_dynamic import QuantizationFP8DynamicTask
