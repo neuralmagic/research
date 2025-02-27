13224076df27e853b6ce40aec37786c881c19d6b
Alexandre Marques <alexandre@neuralmagic.com>
2025-02-26 21:31:45 -0500

    fix: update HPO dependencies and refactor push_to_hf function to use huggingface_hub

--- src/automation/hpo/base_hpo.py
+++ src/automation/hpo/base_hpo.py
@@ -10,7 +10,7 @@ class BaseHPO(BaseTask):
     hpo_packages = [
         "optuna",
         "hpbandster",
-        "transformers",
+        "huggingface_hub",
     ]
 
     def __init__(
--- src/automation/hpo/callbacks/hf.py
+++ src/automation/hpo/callbacks/hf.py
@@ -1,14 +1,18 @@
 from clearml import Task
 from typing import Union
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def push_to_hf(task: Union[Task, str]):
+    from huggingface_hub import HfApi
+
     if isinstance(task, str):
         task = Task.get_task(task_id=task)
     
     model = task.get_models()["output"][0]
     path = model.get_local_copy()
-    hf_model = AutoModelForCausalLM.from_pretrained(path)
-    tokenizer = AutoTokenizer.from_pretrained(path)
-    hf_model.push_to_hub(f"nm-research-models/{model.name}_{model.id}", private=True)
-    tokenizer.push_to_hub(f"nm-research-models/{model.name}_{model.id}", private=True)
+
+    api = HfApi()
+    api.upload_large_folder(
+        folder_path=path,
+        repo_id=f"nm-research-models/{model.name}_{model.id}",
+        repo_type="model"
+    )
