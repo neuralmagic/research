b8d7792ee4d9d3d409f92717b42b2e9207e2f609
Alexandre Marques <alexandre@neuralmagic.com>
2025-03-05 14:02:11 -0500

    refactor: replace LeaderboardTask with LMEvalTask and update execution methods

--- examples/leaderboard_example.py
+++ examples/leaderboard_example.py
@@ -1,12 +1,13 @@
-from automation.standards import LeaderboardTask
+from automation.tasks import LMEvalTask
 
-task = LeaderboardTask(
+task = LMEvalTask(
     project_name="alexandre_debug",
     task_name="test_leaderboard_task",
     model_id="meta-llama/Llama-3.2-1B-Instruct",
     limit=10,
     model_args="gpu_memory_utilization=0.4,enable_chunked_prefill=True",
+    config="leaderboard",
 )
 
-#task.execute_remotely("oneshot-a6000x1")
-task.execute_locally()
+task.execute_remotely("oneshot-a100x1")
+#task.execute_locally()
