662d372a25df768d099952c20bffd42bf8a55549
Alexandre Marques <alexandre@neuralmagic.com>
2025-03-05 13:58:53 -0500

    refactor: update task imports and enhance leaderboard configuration for clarity and functionality

--- examples/openllm_example.py
+++ examples/openllm_example.py
@@ -1,12 +1,13 @@
-from automation.standards import OpenLLMTask
+from automation.tasks import LMEvalTask
 
-task = OpenLLMTask(
+task = LMEvalTask(
     project_name="alexandre_debug",
     task_name="test_openllm_task",
     model_id="meta-llama/Llama-3.2-1B-Instruct",
     limit=10,
-    model_args="gpu_memory_utilization=0.4,enable_chunked_prefill=True"
+    model_args="gpu_memory_utilization=0.4,enable_chunked_prefill=True",
+    config="openllm",
 )
 
-#task.execute_remotely("oneshot-a6000x1")
-task.execute_locally()
+task.execute_remotely("oneshot-a100x1")
+#task.execute_locally()
--- src/automation/standards/__init__.py
+++ src/automation/standards/__init__.py
@@ -1,19 +1,27 @@
-from automation.standards.benchmarking.benchmarking_code_completion import BenchmarkingCodeCompletionTask
-from automation.standards.benchmarking.benchmarking_chat import BenchmarkingChatTask
-from automation.standards.benchmarking.benchmarking_instruction import BenchmarkingInstructionTask
-from automation.standards.benchmarking.benchmarking_docstring_generation import BenchmarkingDocstringGenerationTask
-from automation.standards.benchmarking.benchmarking_summarization import BenchmarkingSummarizationTask
-from automation.standards.benchmarking.benchmarking_long_rag import BenchmarkingLongRAGTask
-from automation.standards.benchmarking.benchmarking_rag import BenchmarkingRAGTask
-from automation.standards.benchmarking.benchmarking_code_fixing import BenchmarkingCodeFixingTask
-from automation.standards.benchmarking.benchmarking_16k import Benchmarking16kTask
-from automation.standards.benchmarking.benchmarking_32k import Benchmarking32kTask
-from automation.standards.benchmarking.benchmarking_64k import Benchmarking64kTask
-from automation.standards.benchmarking.benchmarking_128k import Benchmarking128kTask
-from automation.standards.evaluations.openllm import OpenLLMTask
-from automation.standards.evaluations.leaderboard import LeaderboardTask
-from automation.standards.compression.quantization_w4a16 import QuantizationW4A16Task
-from automation.standards.compression.quantization_w8a8 import QuantizationW8A8Task
-from automation.standards.compression.quantization_fp8_dynamic import QuantizationFP8DynamicTask
-from automation.standards.compression.pipeline_w4a16 import QuantizationW4A16Pipeline
-from automation.standards.compression.pipeline_w8a8 import QuantizationW8A8Pipeline
+# from automation.standards.benchmarking.benchmarking_code_completion import BenchmarkingCodeCompletionTask
+# from automation.standards.benchmarking.benchmarking_chat import BenchmarkingChatTask
+# from automation.standards.benchmarking.benchmarking_instruction import BenchmarkingInstructionTask
+# from automation.standards.benchmarking.benchmarking_docstring_generation import BenchmarkingDocstringGenerationTask
+# from automation.standards.benchmarking.benchmarking_summarization import BenchmarkingSummarizationTask
+# from automation.standards.benchmarking.benchmarking_long_rag import BenchmarkingLongRAGTask
+# from automation.standards.benchmarking.benchmarking_rag import BenchmarkingRAGTask
+# from automation.standards.benchmarking.benchmarking_code_fixing import BenchmarkingCodeFixingTask
+# from automation.standards.benchmarking.benchmarking_16k import Benchmarking16kTask
+# from automation.standards.benchmarking.benchmarking_32k import Benchmarking32kTask
+# from automation.standards.benchmarking.benchmarking_64k import Benchmarking64kTask
+# from automation.standards.benchmarking.benchmarking_128k import Benchmarking128kTask
+# from automation.standards.evaluations.openllm import OpenLLMTask
+# from automation.standards.evaluations.leaderboard import LeaderboardTask
+# from automation.standards.compression.quantization_w4a16 import QuantizationW4A16Task
+# from automation.standards.compression.quantization_w8a8 import QuantizationW8A8Task
+# from automation.standards.compression.quantization_fp8_dynamic import QuantizationFP8DynamicTask
+# from automation.standards.compression.pipeline_w4a16 import QuantizationW4A16Pipeline
+# from automation.standards.compression.pipeline_w8a8 import QuantizationW8A8Pipeline
+
+import pathlib
+
+STANDARD_CONFIGS = {}
+CURRENT_DIR = pathlib.Path(__file__).parent
+
+for file_path in pathlib.Path(CURRENT_DIR).rglob("*.yaml"):
+    STANDARD_CONFIGS[file_path.stem] = file_path.resolve()
