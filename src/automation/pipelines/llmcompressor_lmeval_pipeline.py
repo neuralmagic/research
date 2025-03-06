ce4e6472a30011bd9d2895b4cd68874a06a4bd47
Alexandre Marques <alexandre@neuralmagic.com>
2025-03-05 23:20:38 -0500

    feat: add LLMCompressorLMEvalPipeline and associated YAML configuration

---
+++ src/automation/pipelines/llmcompressor_lmeval_pipeline.py
@@ -0,0 +1,99 @@
+from automation.pipelines import Pipeline
+from automation.standards import LLMCompressorTask, OpenLLMTask
+from automation.configs import DEFAULT_DOCKER_IMAGE
+from typing import List
+
+class LLMCompressorLMEvalPipeline(Pipeline):
+    def __init__(
+        self,
+        project_name: str,
+        pipeline_name: str,
+        model_id: str,
+        execution_queues: List[str],
+        version: str="1.0.0",
+        docker_image: str=DEFAULT_DOCKER_IMAGE,
+        parameters: Optional[dict]=None,
+        llmcompressor_kwargs: dict={},
+        lmeval_kwargs: dict={},
+        config: Optional[str]=None,
+    ):
+        super().__init__(
+            project_name, 
+            pipeline_name, 
+            version, 
+            docker_image,
+        )
+        
+        # Process config
+        config_kwargs = self.process_config(config)
+        if "parameters" in config_kwargs:
+            if parameters is not None:
+                raise ValueError("parameters specified in config. Can't specify again in pipeline instantiation.")
+            else:
+                parameters = config_kwargs.pop("parameters")
+
+        llmcompressor_kwargs.update(config_kwargs.pop("llmcompressor_kwargs", {}))
+        lmeval_kwargs.update(config_kwargs.pop("lmeval_kwargs", {}))
+
+        self.model_id = model_id
+        self.execution_queues = execution_queues
+        self.llmcompressor_kwargs = llmcompressor_kwargs
+        self.lmeval_kwargs = lmeval_kwargs
+        self._parameters = parameters
+
+        self.add_llmcompressor_step()
+        self.add_lmeval_step()
+        
+    
+    def add_llmcompressor_step(self):
+        parameter_override = {}
+        for parameter_name, parameter_kwargs in self._parameters.items():
+            recipe_arg = parameter_kwargs.pop("recipe_arg", False)
+            self.add_parameter(parameter_name, **parameter_kwargs)
+
+            if recipe_arg:
+                parameter_override[f"Args/recipe_args/{parameter_name}"] = f"${{pipeline.{parameter_name}}}"
+
+        step1_name = self.pipeline_name + "_" + self.llmcompressor_kwargs.pop("name")
+        step1 = QuantizationW4A16Task(
+            project_name=self.project_name,
+            task_name=step1_name + "_draft",
+            model_id=self.model_id,
+            **self.llmcompressor_kwargs,
+        )
+        step1.create_task()
+        self.add_step(
+            name=step1_name,
+            base_task_id=step1.id,
+            execution_queues=self.execution_queues[0],
+            parameter_override=parameter_override,
+            monitor_models=[step1.get_arguments()["Args"]["save_directory"]],
+            monitor_artifacts=["recipe"],
+        )
+
+
+    def add_lmeval_step(self):
+        step1_name = self.pipeline_name + "_" + self.llmcompressor_kwargs.pop("name")
+        step1_model_id = f"${{{step1_name}_quantization.models.output.-1.id}}"
+
+        step2_name = self.pipeline_name + "_" + self.lmeval_kwargs.pop("name")
+        step2 = OpenLLMTask(
+            project_name=self.project_name,
+            task_name=step2_name + "_draft",
+            model_id="dummy",
+            clearml_model=True,
+            **self.openllm_kwargs,
+        )
+        step2.create_task()
+
+        monitor_metrics = [tuple(entry) for entry in self.lmeval_kwargs["monitor_metrics"]]
+
+        self.add_step(
+            name=step2_name,
+            base_task_id = step2.id,
+            parents=[step1_name],
+            execution_queue=self.execution_queues[1],
+            parameter_override={"Args/model_id": step1_model_id},
+            monitor_metrics=monitor_metrics,
+        )
+        
