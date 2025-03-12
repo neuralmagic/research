312c9ba19e6cee27ea0a220eeaeb9055c8826a4f
Alexandre Marques <alexandre@neuralmagic.com>
2025-03-12 17:33:15 -0400

    refactor: update model_id to model in guidellm_example.py for consistency

--- examples/guidellm_example.py
+++ examples/guidellm_example.py
@@ -3,7 +3,7 @@ from automation.tasks import GuideLLMTask
 task = GuideLLMTask(
     project_name="alexandre_debug",
     task_name="test_guidellm_task",
-    model_id="meta-llama/Llama-3.2-1B-Instruct",
+    model="meta-llama/Llama-3.2-1B-Instruct",
     rate_type="throughput",
     backend="aiohttp_server",
     GUIDELLM__MAX_CONCURRENCY=256,
