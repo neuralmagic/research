e603a8336d7d7d3b459dc8431c976ef4f82d78b4
Alexandre Marques <alexandre@neuralmagic.com>
2025-03-24 23:37:36 -0400

    refactor: update calibration dataset loading to use new message loading functions

--- src/automation/datasets/calibration.py
+++ src/automation/datasets/calibration.py
@@ -1,24 +1,27 @@
-from automation.datasets.utils import load_text_dataset, load_vision_dataset, mix_datasets
+from automation.datasets.utils import load_llm_messages, load_vllm_messages
+from datasets import interleave_datasets
 
 DATASET_PATH = "neuralmagic/calibration"
 TEXT_SUBSET = "LLM"
 VISION_SUBSET = "VLLM"
 
 def load_calibration_dataset(
-        vision_samples=None,
-        text_samples=None,
-        num_samples=None,
-        max_seq_len=None, 
-        vision_loader=None,
-        tokenizer=None,
-        processor=None,
+    vision_samples=None,
+    text_samples=None,
+    num_samples=None,
+    max_seq_len=None, 
+    vision_loader=None,
+    tokenizer=None,
+    processor=None,
 ):
-    
     if vision_samples is None and text_samples is None and num_samples is not None:
         text_samples = num_samples
 
-    if text_samples is not None and text_samples > 0:
-        text_dataset = load_text_dataset(
+    if vision_samples is None:
+        vision_samples = 0
+
+    if text_samples > 0 and vision_samples == 0:
+        return load_llm_messages(
             DATASET_PATH, 
             TEXT_SUBSET, 
             split="train", 
@@ -27,27 +30,26 @@ def load_calibration_dataset(
             tokenizer=tokenizer,
         )
 
-    if vision_samples is not None and vision_samples > 0:
-        if vision_loader is None:
-            vision_dataset = load_text_dataset(
-                DATASET_PATH, 
-                TEXT_SUBSET, 
-                split="train", 
-                num_samples=text_samples, 
-                processor=processor,
-            )
-        else:
-            vision_dataset = vision_loader(
-                DATASET_PATH, 
-                VISION_SUBSET, 
-                split="train", 
-                num_samples=vision_samples,
-                processor=processor,
-            )
+    if vision_loader is None:
+        vision_loader = load_vllm_messages
 
-    if vision_samples > 0 and text_samples > 0:
-        return mix_datasets(vision_dataset, text_dataset)
-    elif vision_samples > 0:
-        return vision_dataset
-    else:
-        return text_dataset
+    vision_dataset = vision_loader(
+        DATASET_PATH, 
+        VISION_SUBSET, 
+        split="train", 
+        num_samples=vision_samples,
+        processor=processor,
+    )
+
+    if text_samples > 0:
+        text_dataset = vision_loader(
+            DATASET_PATH, 
+            TEXT_SUBSET, 
+            split="train", 
+            num_samples=text_samples,
+            processor=processor,
+        )
+
+        return interleave_datasets([vision_dataset, text_dataset])
+
+    return vision_dataset
