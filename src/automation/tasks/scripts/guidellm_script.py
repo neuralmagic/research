75b6e690eaf2e441d766f29ecabe6aef1c52ebd9
Shubhra Pandit <shubhra@neuralmagic.com>
2025-04-21 18:39:58 +0000

    Update for missing target

--- src/automation/tasks/scripts/guidellm_script.py
+++ src/automation/tasks/scripts/guidellm_script.py
@@ -1,5 +1,6 @@
 
 import os
+os.environ["CLEARML_NO_FRAMEWORKS"] = "1"
 from clearml import Task
 from automation.utils import resolve_model_id, cast_args, kill_process_tree
 from automation.vllm import start_vllm_server
