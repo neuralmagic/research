0022b24663ff115619a8acbef8751af113255b7d
Alexandre Marques <alexandre@neuralmagic.com>
2025-04-25 20:07:34 +0000

    update libraries

--- src/automation/tasks/llmcompressor.py
+++ src/automation/tasks/llmcompressor.py
@@ -6,7 +6,10 @@ import os
 import yaml
 
 class LLMCompressorTask(BaseTask):
-    llmcompressor_packages = ["git+https://github.com/vllm-project/llm-compressor.git@traceable_mistral3"]
+    llmcompressor_packages = [
+        "git+https://github.com/vllm-project/llm-compressor.git@traceable_mistral3",
+        "git+https://github.com/huggingface/transformers.git",
+    ]
 
     def __init__(
         self,
