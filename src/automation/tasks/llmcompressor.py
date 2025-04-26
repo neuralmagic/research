9f9ba8114072aedc2a3a30a72a5305c375250be8
Alexandre Marques <alexandre@neuralmagic.com>
2025-04-26 22:44:53 +0000

    fix configuration name

--- src/automation/tasks/llmcompressor.py
+++ src/automation/tasks/llmcompressor.py
@@ -118,7 +118,7 @@ class LLMCompressorTask(BaseTask):
         if self.dataset_loader is not None:
             configs["dataset loader"] = serialize_callable(self.dataset_loader)
         if self.data_collator is not None:
-            configs["dataset collator"] = serialize_callable(self.data_collator)
+            configs["data collator"] = serialize_callable(self.data_collator)
         return configs
 
 
