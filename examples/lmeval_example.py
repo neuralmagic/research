a425d4305e18009e933238f3c72104d0cd31a812
chibu <>
2025-06-27 10:54:31 +0000

    update branch

--- examples/lmeval_example.py
+++ examples/lmeval_example.py
@@ -3,6 +3,7 @@ from automation.tasks import LMEvalTask
 task = LMEvalTask(
     project_name="alexandre_debug",
     task_name="test_lmeval_task",
+    branch = "update_guidellm",
     model_id="meta-llama/Llama-3.2-1B-Instruct",
     tasks="gsm8k",
     model_args="dtype=auto,max_model_len=8192",
