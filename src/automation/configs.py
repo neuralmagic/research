b8b51e9b8c4e00606346b5301fe38bab8d5845c4
chibu <>
2025-07-02 18:04:07 +0000

    test default branch change

--- src/automation/configs.py
+++ src/automation/configs.py
@@ -2,4 +2,5 @@
 DEFAULT_DOCKER_IMAGE = "498127099666.dkr.ecr.us-east-1.amazonaws.com/mlops/k8s-research-cuda12_8:latest"
 #DEFAULT_OUTPUT_URI = "gs://neuralmagic-clearml"
 DEFAULT_OUTPUT_URI = "http://10.128.20.60:8081"
-DEFAULT_RESEARCH_BRANCH = "main"
+#DEFAULT_RESEARCH_BRANCH = "main"
+DEFAULT_RESEARCH_BRANCH = "update_guidellm"
