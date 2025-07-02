16b83bc8542b10417da077b5867b63d83f9f9582
chibu <>
2025-07-02 16:30:51 +0000

    base complete

--- examples/guidellm_example.py
+++ examples/guidellm_example.py
@@ -14,7 +14,9 @@ task = GuideLLMTask(
     #target="http://fed73cc1-us-east.lb.appdomain.cloud/v1",
     data_type="emulated",
     max_seconds=30,
-    data="prompt_tokens=512,generated_tokens=256",
+    #data="{'prompt_tokens': 512, 'generated_tokens': 256, 'output_tokens' : 256}",
+    data="prompt_tokens=512,generated_tokens=256,output_tokens=256",
+    #data="prompt_tokens=512,generated_tokens=256",
     branch = "update_guidellm",
     #vllm_kwargs={"enable-chunked-prefill": True}
 )
