3b0d86c0655adc3f3a838592b5e57064d1e5f7ca
chibu <>
2025-07-03 09:29:24 +0000

    revert lmeval and setup.py, update vllm server log

--- examples/lmeval_example.py
+++ examples/lmeval_example.py
@@ -3,13 +3,11 @@ from automation.tasks import LMEvalTask
 task = LMEvalTask(
     project_name="alexandre_debug",
     task_name="test_lmeval_task",
-    branch = "update_guidellm",
     model_id="meta-llama/Llama-3.2-1B-Instruct",
     tasks="gsm8k",
     model_args="dtype=auto,max_model_len=8192",
     batch_size="auto",    
 )
 
-#task.execute_remotely("oneshot-a100x1")
-task.execute_remotely("remote-upgrade-default")
+task.execute_remotely("oneshot-a100x1")
 #task.execute_locally()
