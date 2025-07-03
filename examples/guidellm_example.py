69638eae4c4ff224d06cf26a513c0fb5951e394f
chibu <>
2025-07-03 14:51:21 +0000

    onpremise settings

--- examples/guidellm_example.py
+++ examples/guidellm_example.py
@@ -1,21 +1,20 @@
-
 from automation.tasks import GuideLLMTask
 
 task = GuideLLMTask(
     project_name="alexandre_debug",
     task_name="test_guidellm_task",
-    #model="meta-llama/Llama-3.2-1B-Instruct",
-    model="Qwen/Qwen2.5-1.5B-Instruct",
+    model="meta-llama/Llama-3.2-1B-Instruct",
     rate_type="throughput",
     backend="aiohttp_server",
     GUIDELLM__MAX_CONCURRENCY=256,
     GUIDELLM__REQUEST_TIMEOUT=21600,
     target="http://localhost:8000/v1",
     max_seconds=30,
-    scenario = "benchmarking_32kz",
+    #scenario = "benchmarking_32k",
     data="prompt_tokens=128,output_tokens=128",
     branch = "update_guidellm",
     vllm_kwargs={"enable-chunked-prefill": True}
 )
 
-task.execute_remotely("remote-upgrade-default")
+task.execute_remotely("oneshot-a100x1")
+#task.execute_locally()
