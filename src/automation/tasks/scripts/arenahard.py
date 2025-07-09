57e8d78882982199dee283adef7aa58749203eff
chibu <>
2025-07-09 10:19:09 +0000

    readd server

--- src/automation/tasks/scripts/arenahard.py
+++ src/automation/tasks/scripts/arenahard.py
@@ -54,7 +54,6 @@ def main():
 
     gpu_count = int(arenahard_args.get("gpu_count", 1)) 
 
-    """
     # Start vLLM server
     server_process, server_initialized, server_log = start_vllm_server(
         vllm_args,
@@ -68,7 +67,6 @@ def main():
         kill_process_tree(server_process.pid)
         task.upload_artifact(name="vLLM server log", artifact_object=server_log)
         raise AssertionError("Server failed to initialize")
-    """
 
     # Parse through environment variables
     for k, v in environment_args.items():
