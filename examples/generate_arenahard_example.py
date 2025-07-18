6cc02f6db1d5da419d6a3fc52be87e5e64f9f689
chibu <>
2025-07-18 10:29:44 +0000

    test with new model

--- examples/generate_arenahard_example.py
+++ examples/generate_arenahard_example.py
@@ -2,21 +2,22 @@ from automation.tasks import ArenaHardGenerateTask
 
 task = ArenaHardGenerateTask(
     project_name="alexandre_debug",
-    task_name="generate_task",
+    task_name="generate_math_task",
     #model="meta-llama/Llama-3.2-1B-Instruct",
-    generate_model="Qwen/Qwen2.5-1.5B-Instruct",
+    #generate_model="Qwen/Qwen2.5-1.5B-Instruct",
+    generate_model="Qwen/Qwen2.5-Math-1.5B-Instruct",
     rate_type="throughput",
     backend="aiohttp_server",
-    GUIDELLM__MAX_CONCURRENCY=256,
-    GUIDELLM__REQUEST_TIMEOUT=21600,
     target="http://localhost:8000/v1",
     max_seconds=30,
     data="prompt_tokens=128,output_tokens=128",
     branch = "arena_upgrade",
     #vllm_kwargs={"enable-chunked-prefill": True}
 
-    generation_config_file='gen_answer_config.yaml',
-    generation_endpoint_file='api_config.yaml',
+    #generation_config_file='gen_answer_config.yaml',
+    generation_config_file='math_answer_config.yaml',
+    #generation_endpoint_file='api_config.yaml',
+    generation_endpoint_file='math_api_config.yaml',
 )
 
 #task.execute_remotely("oneshot-a100x1")
