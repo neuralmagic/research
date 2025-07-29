e033ecb0fb13180545f124d50bbd0ec9e5fc54c7
chibu <>
2025-07-29 09:33:05 +0000

    update vllm

--- src/automation/tasks/arenahard_generate.py
+++ src/automation/tasks/arenahard_generate.py
@@ -12,8 +12,9 @@ class ArenaHardGenerateTask(BaseTask):
     arenahard_packages = [
         ARENAHARD_PACKAGE,
         "hf_xet",
-        "vllm==0.9.0",
-        "huggingface-hub==0.34.2",
+        "vllm==0.9.1",
+        "huggingface-hub",
+        #"huggingface-hub==0.34.2",
     ]
 
     def __init__(
