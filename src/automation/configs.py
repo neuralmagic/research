07fc3316088ffbbf2ac3708c1add791300d70c26
chibu <>
2025-08-12 11:09:21 +0000

    Update base image to quay

--- src/automation/configs.py
+++ src/automation/configs.py
@@ -1,4 +1,4 @@
-DEFAULT_DOCKER_IMAGE = "498127099666.dkr.ecr.us-east-1.amazonaws.com/mlops/k8s-research-cuda12_8:latest"
+DEFAULT_DOCKER_IMAGE = "quay.io/nmmlops/mlops/k8s-research-cuda12_8:latest"
 DEFAULT_OUTPUT_URI = "gs://neuralmagic-clearml"
 DEFAULT_RESEARCH_BRANCH = "main"
 DEFAULT_GUIDELLM_SCENARIO = "chat"
