913869872f24472e35223600c2cf43016c84aec9
Chibukach <33231716+Chibukach@users.noreply.github.com>
2025-08-13 13:16:40 -0400

    Update default configs.py

--- src/automation/configs.py
+++ src/automation/configs.py
@@ -1,2 +1,4 @@
 DEFAULT_DOCKER_IMAGE = "quay.io/nmmlops/mlops/k8s-research-cuda12_8:latest"
 DEFAULT_OUTPUT_URI = "gs://neuralmagic-clearml"
+DEFAULT_RESEARCH_BRANCH = "main"
+DEFAULT_GUIDELLM_SCENARIO = "chat"
