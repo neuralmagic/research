7c46c7d1ad27dd5b6f07cd48fd8415c6259e5945
Chibukach <33231716+Chibukach@users.noreply.github.com>
2025-08-14 15:26:29 -0400

    Update configs.py
    Removed DEFAULT GUIDELLM SCENARIO from config.py

--- src/automation/configs.py
+++ src/automation/configs.py
@@ -1,4 +1,3 @@
 DEFAULT_DOCKER_IMAGE = "quay.io/nmmlops/mlops/k8s-research-cuda12_8:latest"
 DEFAULT_OUTPUT_URI = "gs://neuralmagic-clearml"
 DEFAULT_RESEARCH_BRANCH = "main"
-DEFAULT_GUIDELLM_SCENARIO = "chat"
