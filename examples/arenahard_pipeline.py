efc0cde94b373f08387ee8bbc26be8cff642ec43
chibu <>
2025-08-20 22:42:15 +0000

    added arenahard examples

--- examples/arenahard_pipeline.py
+++ examples/arenahard_pipeline.py
@@ -2,10 +2,11 @@ from automation.pipelines import Pipeline
 from automation.tasks import ArenaHardGenerateTask, ArenaHardJudgeTask
 
 step1 = ArenaHardGenerateTask(
-    project_name="simple_debug",
-    task_name="generate_math_task_1",
+    project_name="small_pipeline_debug",
+    task_name="generate_pipeline_task1",
     packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.0"],
-    generate_model="Qwen/Qwen2.5-Math-1.5B-Instruct",
+    generate_model="meta-llama/Llama-3.2-1B-Instruct",
+    question_size = "small",
     rate_type="throughput",
     backend="aiohttp_server",
     target="http://localhost:8000/v1",
@@ -14,17 +15,17 @@ step1 = ArenaHardGenerateTask(
     branch = "arena_upgrade",
     vllm_kwargs={"enable-chunked-prefill": True},
     max_tokens = 1300, 
-
 )
 
 step1.create_task()
 
 
 step2 = ArenaHardJudgeTask(
-    project_name="simple_debug",
-    task_name="test_judge_task_1",
+    project_name="small_pipeline_debug",
+    task_name="judge_pipeline_task1",
     packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.0"],
-    judgement_model="meta-llama/Meta-Llama-3-8B-Instruct",
+    judgement_model ="Qwen/Qwen2-7B-Instruct",
+    question_size = "small",
     rate_type="throughput",
     backend="aiohttp_server",
     target="http://localhost:8000/v1",
@@ -38,13 +39,13 @@ step2 = ArenaHardJudgeTask(
 step2.create_task()
 
 pipeline = Pipeline(
-    project_name="simple_debug",
+    project_name="small_pipeline_debug",
     pipeline_name="pipeline_arenahard",
 )
 
 
 pipeline.add_step(
-    name="pipeline_arenahard_generate_step1",
+    name="pipeline_arenahard_gen_step_1",
     base_task_id = step1.id,
     execution_queue="oneshot-a100x1",
     #monitor_models=[step1.get_arguments()["Args"]["save_directory"]],
@@ -53,16 +54,18 @@ pipeline.add_step(
 
 
 
-config_override = {**step1.get_configurations()['ArenaHard'], **{"answer_task_name": step1.name}, **{"answer_project_name" : step1.project_name } }
+config_override = {**step1.get_configurations()['ArenaHard'], **{"answer_project_name": "small_pipeline_debug" }, **{"answer_task_name" : pipeline.steps[0][1]['name'] }}
 
 pipeline.add_step(
-    name="pipeline_arenahard_judgement_step2",
+    name="pipeline_arenahard_judge_step_2",
     base_task_id = step2.id,
-    parents=["pipeline_arenahard_generate_step1"],
+    parents=["pipeline_arenahard_gen_step_1"],
     execution_queue="oneshot-a100x1",
     #parameter_override={"Args/model_id": "${pipeline_arenahard_generate_step1.models.output.-1.id}"},
     configuration_overrides={"ArenaHard" : config_override },
     #monitor_metrics=[("gsm8k", "exact_match,strict-match")],
 )
 
-pipeline.execute_remotely("oneshot-a100x1")
+
+pipeline.execute_remotely()
+#pipeline.execute_remotely("oneshot-a100x1")
