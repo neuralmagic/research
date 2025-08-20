43beabb2f3c448bf5c3c4f657ecc928da133466a
chibu <>
2025-08-20 12:58:36 +0000

    update judgement

--- src/automation/tasks/scripts/arenahard_judgement_script.py
+++ src/automation/tasks/scripts/arenahard_judgement_script.py
@@ -131,7 +131,7 @@ def main():
             # use default 03-mini answers
             shutil.copy( os.path.join(answer_dir,"o3-mini-2025-01-31.jsonl"),os.path.join(answer_dir, f"{model_name}.jsonl"))
 
-        if arenahard_judgement_args.get("category","") == "quick" :
+        if arenahard_judgement_args.get("question_size","") == "small" :
             shutil.copy( os.path.join(arenahard_dir,"shortquestion.jsonl"),os.path.join(arenahard_dir, "question.jsonl"))
     
         print ("Running arena hard generate")
