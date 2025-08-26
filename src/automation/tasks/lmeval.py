4a2df4cebe1a64c7045dbf2c2de6687a28cfc7f7
chibu <>
2025-08-26 20:29:20 +0000

    unpin in tasks

--- src/automation/tasks/lmeval.py
+++ src/automation/tasks/lmeval.py
@@ -6,8 +6,8 @@ import os
 class LMEvalTask(BaseTask):
 
     lmeval_packages = [
-        #"vllm",
-        "vllm==0.10.1",
+        "vllm",
+        #"vllm==0.10.1",
         "git+https://github.com/EleutherAI/lm-evaluation-harness.git",
         "numpy==2.1",
         "hf_xet",
@@ -43,7 +43,8 @@ class LMEvalTask(BaseTask):
             for package in packages:
                 if "vllm" in package:
                     #self.lmeval_packages.pop("vllm")
-                    self.lmeval_packages.remove("vllm==0.10.1")
+                    self.lmeval_packages.remove("vllm")
+                    #self.lmeval_packages.remove("vllm==0.10.1")
                 if "lm-evaluation-harness" in package:
                     self.lmeval_packages.pop("git+https://github.com/EleutherAI/lm-evaluation-harness.git")
             packages = list(set(packages + self.lmeval_packages))
