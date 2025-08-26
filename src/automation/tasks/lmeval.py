91a07f386577abe2b972c32caeb8fa33adc8ab42
chibu <>
2025-08-26 16:56:59 +0000

    swap pop for remove

--- src/automation/tasks/lmeval.py
+++ src/automation/tasks/lmeval.py
@@ -41,7 +41,8 @@ class LMEvalTask(BaseTask):
             # use that version instead of the latest
             for package in packages:
                 if "vllm" in package:
-                    self.lmeval_packages.pop("vllm")
+                    #self.lmeval_packages.pop("vllm")
+                    self.lmeval_packages.remove("vllm")
                 if "lm-evaluation-harness" in package:
                     self.lmeval_packages.pop("git+https://github.com/EleutherAI/lm-evaluation-harness.git")
             packages = list(set(packages + self.lmeval_packages))
