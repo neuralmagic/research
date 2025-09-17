7f4a41c83162ec0c4cb366e32aaa9f841d3d4ed3
chibu <>
2025-09-17 22:23:16 +0000

    updated examples with v0.1 support

--- examples/arenahard_pipeline.py
+++ examples/arenahard_pipeline.py
@@ -1,45 +1,49 @@
 from automation.pipelines import Pipeline
 from automation.tasks import ArenaHardGenerateTask, ArenaHardJudgeTask
 
+arena_hard_project_name="dsmall_pipeline_debug"
 step1 = ArenaHardGenerateTask(
-    project_name="small_pipeline_debug",
+    project_name= arena_hard_project_name,
     task_name="generate_pipeline_task1",
-    packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.0"],
+    packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.1.1"],
     generate_model="meta-llama/Llama-3.2-1B-Instruct",
     question_size = "small",
     rate_type="throughput",
     backend="aiohttp_server",
     target="http://localhost:8000/v1",
     server_wait_time = 600, 
-    data="prompt_tokens=128,output_tokens=128",
+    bench_name = "arena-hard-v2.0",
+    #bench_name = "arena-hard-v0.1",
     branch = "arena_upgrade",
     vllm_kwargs={"enable-chunked-prefill": True},
-    max_tokens = 1300, 
+    max_tokens = 1024, 
 )
 
 step1.create_task()
 
 
 step2 = ArenaHardJudgeTask(
-    project_name="small_pipeline_debug",
+    project_name= arena_hard_project_name,
     task_name="judge_pipeline_task1",
-    packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.0"],
-    judgement_model ="Qwen/Qwen2-7B-Instruct",
+    packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.1.1"],
+    #judgement_model ="Qwen/Qwen2-7B-Instruct",
+    judgement_model = "openai/gpt-oss-120b",
     question_size = "small",
     rate_type="throughput",
     backend="aiohttp_server",
     target="http://localhost:8000/v1",
     server_wait_time = 600, 
-    data="prompt_tokens=128,output_tokens=128",
+    bench_name = "arena-hard-v2.0",
+    #bench_name = "arena-hard-v0.1",
     branch = "arena_upgrade",
     vllm_kwargs={"enable-chunked-prefill": True},
-    max_tokens = 1300, 
+    max_tokens = 1024, 
 )
 
 step2.create_task()
 
 pipeline = Pipeline(
-    project_name="small_pipeline_debug",
+    project_name= arena_hard_project_name,
     pipeline_name="pipeline_arenahard",
 )
 
@@ -54,13 +58,13 @@ pipeline.add_step(
 
 
 
-config_override = {**step1.get_configurations()['ArenaHard'], **{"answer_project_name": "small_pipeline_debug" }, **{"answer_task_name" : pipeline.steps[0][1]['name'] }}
+config_override = {**step1.get_configurations()['ArenaHard'], **{"answer_project_name": arena_hard_project_name}, **{"answer_task_name" : pipeline.steps[0][1]['name'] }}
 
 pipeline.add_step(
     name="pipeline_arenahard_judge_step_2",
     base_task_id = step2.id,
     parents=["pipeline_arenahard_gen_step_1"],
-    execution_queue="oneshot-a100x1",
+    execution_queue="oneshot-a100x4",
     #parameter_override={"Args/model_id": "${pipeline_arenahard_generate_step1.models.output.-1.id}"},
     configuration_overrides={"ArenaHard" : config_override },
     #monitor_metrics=[("gsm8k", "exact_match,strict-match")],
--- examples/generate_arenahard_example.py
+++ examples/generate_arenahard_example.py
@@ -1,19 +1,20 @@
 from automation.tasks import ArenaHardGenerateTask
 
 task = ArenaHardGenerateTask(
-    project_name="simple_debug",
-    task_name="generate_math_task_1",
-    packages = ["huggingface-hub==0.34.3", "triton==3.3.1","vllm==0.10.0"],
-    generate_model="meta-llama/Llama-3.2-1B-Instruct",
+    project_name="gpt_arena_debug",
+    task_name="generate_math_task_gpt",
+    packages = ["huggingface-hub==0.34.3", "triton==3.3.1", "vllm==0.10.1.1"],
+    generate_model= "RedHatAI/Qwen2.5-0.5B-Instruct-quantized.w8a8",
+    #generate_model= "openai/gpt-oss-120b",
     question_size = "small",
     rate_type="throughput",
     backend="aiohttp_server",
     target="http://localhost:8000/v1",
-    data="prompt_tokens=128,output_tokens=128",
+    bench_name = "arena-hard-v2.0",
+    #bench_name = "arena-hard-v0.1",
     branch = "arena_upgrade",
     vllm_kwargs={"enable-chunked-prefill": True},
-    max_tokens = 1300, 
-
+    max_tokens = 1024, 
 )
 
 task.execute_remotely("oneshot-a100x1")
