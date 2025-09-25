e3bf77e9e67eff9f4bbcf524135fbaa61e6e114a
Alexandre Marques <alexandre@neuralmagic.com>
2025-09-25 10:50:29 -0400

    Add apertus smoothquant mappings

--- src/automation/standards/compression/smoothquant_mappings.py
+++ src/automation/standards/compression/smoothquant_mappings.py
@@ -9,4 +9,8 @@ MAPPINGS_PER_MODEL_CONFIG = {
         [["re:.*qkv_proj"], "re:.*input_layernorm"],
         [["re:.*gate_up_proj"], "re:.*post_attention_layernorm"],
     ],
+    "apertus": [
+        [["re:.*q_proj", "re:.*k_proj", "re:.*v_proj"], "re:.*attention_layernorm"],
+        [["re:.*up_proj"], "re:.*feedforward_layernorm"],
+    ],
 }
