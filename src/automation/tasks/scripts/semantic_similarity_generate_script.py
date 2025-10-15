5e5276c8fd8a6854a5cc48dc3e3005031dae2273
chibu <>
2025-10-15 17:16:32 +0000

    base method added

--- src/automation/tasks/scripts/semantic_similarity_generate_script.py
+++ src/automation/tasks/scripts/semantic_similarity_generate_script.py
@@ -5,7 +5,7 @@ from datasets import load_dataset
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 
-#from automation.utils import resolve_reference_model_id, parse_argument, load_callable_configuration
+from automation.utils import resolve_reference_model_id, parse_argument, load_callable_configuration
 
 try:
     from clearml import OutputModel, Task
@@ -13,100 +13,105 @@ try:
 except ImportError:
     clearml_available = False
 
-"""
-def llmcompressor_main(
+
+OUTPUT_DIR = os.path.join(os.getcwd(), "outputs")
+
+def parse_argument(a,b):
+    return a
+
+
+def make_alpaca_platypus_prompt(sample):
+    print("Using Alpaca / Platypus style prompt")
+    instruction = sample["instruction"].strip()
+    input_text = sample.get("input", "").strip()
+    prompt = (
+        f"### Instruction:\n{instruction}\n\n"
+        f"### Input:\n{input_text if input_text else 'N/A'}\n\n"
+        f"### Response:\n"
+    )
+
+    return prompt
+
+
+def make_tulu_prompt(sample):
+    print("Using Tulu / OASST style prompt")
+    msgs = []
+    for m in sample["messages"]:
+        role = m.get("role", "user")
+        content = m.get("content", "").strip()
+        msgs.append(f"{role.upper()}: {content}")
+    joined = "\n".join(msgs)
+    prompt = f"### Conversation:\n{joined}\n\n### Response:\n"
+
+    return prompt
+
+
+def make_default_prompt(sample):
+    print("Using default prompt")
+    prompt = f"### Input:\n{json.dumps(sample)}\n\n### Response:\n"
+    return prompt
+
+
+def semantic_similarity_generate_main(
     reference_model_id,
-    model_class,
     trust_remote_code,
-    recipe,
     dataset_args,
-    dataset_loader,
-    dataset_name,
     max_model_len,
-    num_samples,
     max_new_tokens,
-    skip_sparsity_compression_stats,
+    num_samples,
     save_directory,
-    data_collator,
 ):
     dtype = "auto"
     device_map = "auto"
 
-    # Load model
-    model_class = getattr(transformers, model_class)
+    from collections import defaultdict
+    all_prompts = []
+    all_samples_dict = defaultdict(list)
 
-    model = model_class.from_pretrained(
-        reference_model_id, 
-        torch_dtype=dtype, 
-        device_map=device_map, 
-        trust_remote_code=trust_remote_code,
+    print(">>> Loading dataset...")
+    for dataset_name,dataset_path in dataset_args.items():
+        print(f">>> Loading dataset {dataset_name}...")
+        dataset = load_dataset(dataset_path, split=f"train[:{num_samples}]")
+        all_samples_dict[dataset_name].extend(dataset)
+
+    for dataset_name,dataset_samples in all_samples_dict.items():
+        print(f">>> Loading values for {dataset_name}...")
+        for sample in dataset_samples:
+            if dataset_name == "alpaca" or (dataset_name == "openplatypus"):
+                prompt = make_alpaca_platypus_prompt(sample)
+            elif dataset_name == "tulu":
+                prompt = make_tulu_prompt(sample)
+            else:
+                print("Using default prompt")
+                prompt = make_default_prompt(sample)
+            all_prompts.append(prompt)
+
+    TEMPERATURE = 0.0
+
+    print(">>> Loading tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained(reference_model_id, trust_remote_code=True)
+
+    print(">>> Initializing vLLM...")
+    llm = LLM(
+        model=reference_model_id,
+        dtype="auto",
+        trust_remote_code=True,
+        tensor_parallel_size=8,
+        enforce_eager=True,
+        enable_chunked_prefill=True,
+        max_model_len=max_model_len
     )
 
-    # Load recipe
-    if isinstance(recipe, str) and os.path.isfile(recipe):
-        with open(recipe, "r", encoding="utf-8") as file:
-            recipe = file.read()
-
-    if dataset_args is not None:
-        if "smoothquant_mappings" in dataset_args and dataset_args["smoothquant_mappings"] in MAPPINGS_PER_MODEL_CONFIG:
-            dataset_args["smoothquant_mappings"] = MAPPINGS_PER_MODEL_CONFIG[dataset_args["smoothquant_mappings"]]
-            
-        for key, value in dataset_args.items():
-            recipe = recipe.replace(f"${key}", str(value))
-        
-    # Load dataset
-    processor = AutoProcessor.from_pretrained(
-        reference_model_id, 
-        trust_remote_code=trust_remote_code,
+    sampling_params = SamplingParams(
+        temperature=TEMPERATURE,
+        max_tokens=max_new_tokens,
+        stop=["### Instruction:", "### Input:", "### Response:"],
     )
 
-    if dataset_loader is None:
-        if dataset_name is None:
-            dataset = None
-        elif dataset_name in SUPPORTED_DATASETS:
-            dataset = SUPPORTED_DATASETS[dataset_name](
-                num_samples=num_samples,
-                max_new_tokens=max_new_tokens,
-                max_model_len=max_model_len,
-                processor=processor,
-            )
-    else:
-        dataset = dataset_loader(
-            dataset_name,
-            num_samples=num_samples,
-            max_new_tokens=max_new_tokens,
-            max_model_len=max_model_len,
-            processor=processor,
-        )
-    
-    num_calibration_samples = 0
-    if num_samples is not None:
-        num_calibration_samples += num_samples
+    print(">>> Running vLLM generation...")
+    outputs = llm.generate(all_prompts, sampling_params)
 
-    if max_new_tokens is not None:
-        num_calibration_samples += max_new_tokens
-
-    kwargs = {}
-    if data_collator is not None:
-        kwargs["data_collator"] = data_collator
-
-    # Apply recipe to the model
-    oneshot(
-        model=model,
-        dataset=dataset,
-        recipe=recipe,
-        max_model_length=max_model_len,
-        num_calibration_samples=num_calibration_samples,
-        **kwargs,
-    )
-
-    # Save model compressed
-    model.save_pretrained(save_directory, save_compressed=True, skip_sparsity_compression_stats=skip_sparsity_compression_stats)
-    processor.save_pretrained(save_directory)
-
-    return recipe
-
-"""
+    return all_prompts, outputs
 
 def main(configurations=None, args=None):
     if clearml_available:
@@ -129,34 +134,33 @@ def main(configurations=None, args=None):
     dataset_args = args.get("dataset_args", None)
     tags = args.get("tags", None)
 
-    """
-
-    dataset_loader_fn = load_callable_configuration("dataset loader", configurations)
-    data_collator_fn = load_callable_configuration("data collator", configurations)
-
-    # Resolve reference_model_id
-    reference_model_id = resolve_reference_model_id(reference_model_id, clearml_model, force_download, model_class)
-
-    recipe = llmcompressor_main(
+    all_prompts, outputs = semantic_similarity_generate_main(
         reference_model_id,
-        model_class,
         trust_remote_code,
-        recipe,
         dataset_args,
-        dataset_loader_fn,
-        dataset_name,
         max_model_len,
-        num_samples,
         max_new_tokens,
-        skip_sparsity_compression_stats,
+        num_samples,
         save_directory,
-        data_collator_fn,
     )
 
+    OUTPUT_FILE = os.path.join(OUTPUT_DIR,f"{reference_model_id.replace('/', '_')}.jsonl")
+    print(">>> Writing outputs to file...")
+    with open(OUTPUT_FILE, "w") as fout:
+        for idx, (prompt, output) in enumerate(zip(all_prompts, outputs)):
+            response = output.outputs[0].text.strip()
+            fout.write(json.dumps({
+                "index": idx,
+                "prompt": prompt,
+                "response": response
+            }) + "\n")
+
+    print(f">>> Completed. Saved {len(outputs)} outputs to {OUTPUT_FILE}")
 
     if clearml_available:
-        task.upload_artifact("recipe", recipe)
+        task.upload_artifact("jsonl_output", OUTPUT_FILE)
 
+        """
         # Upload model to ClearML
         clearml_model_object = OutputModel(
             task=task, 
@@ -165,7 +169,7 @@ def main(configurations=None, args=None):
            tags=[tags] if isinstance(tags, str) else tags or []
         )
         clearml_model_object.update_weights(weights_filename=save_directory, auto_delete_file=False)
-    """
+        """
 
 if __name__ == '__main__':
     main()
