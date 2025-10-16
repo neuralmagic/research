21f54d098ff62ed6caa2cf7b998521ba4227c80a
chibu <>
2025-10-16 15:33:45 +0000

    base scoring script

--- src/automation/tasks/scripts/semantic_similarity_score_script.py
+++ src/automation/tasks/scripts/semantic_similarity_score_script.py
@@ -1,11 +1,9 @@
 import json
-from tqdm import tqdm
-import os
 from bert_score import score
 from rouge_score import rouge_scorer
 from sentence_transformers import SentenceTransformer, util
-
-#from automation.utils import parse_argument
+import os
+from automation.utils import parse_argument
 
 try:
     from clearml import OutputModel, Task
@@ -13,75 +11,62 @@ try:
 except ImportError:
     clearml_available = False
 
-OUTPUT_DIR = os.path.join(os.getcwd(), "outputs")
-
-
-def parse_argument(
-    a,
-    b,
-):
-    return a
-
+SCORING_DIR = os.path.join(os.getcwd(), "outputs")
 
 def semantic_similarity_score_main(
-    trust_remote_code,
+    reference_file,
+    candidate_file,
     sts_model_id,
     rouge_scores,
-    save_directory,
 ):
-    from collections import defaultdict
-    all_prompts = []
-    all_samples_dict = defaultdict(list)
+    # Load reference and candidate data
+    with open(reference_file, "r") as f_ref, open(candidate_file, "r") as f_cand:
+        reference_data = [json.loads(line) for line in f_ref]
+        candidate_data = [json.loads(line) for line in f_cand]
+    
+    assert len(reference_data) == len(candidate_data), "Mismatched number of entries!"
 
-    print(">>> Loading dataset...")
-    for dataset_name,dataset_path in sts_model_id.items():
-        print(f">>> Loading dataset {dataset_name}...")
-        dataset = load_dataset(dataset_path, split=f"train[:{candidate_model_task_name}]")
-        all_samples_dict[dataset_name].extend(dataset)
+    # Extract answers
+    references = [ref.get("output") or ref["response"] for ref in reference_data]
+    candidates = [cand["response"] for cand in candidate_data]
+    
+    # Load models
+    sts_model = SentenceTransformer(sts_model_id)
+    rouge = rouge_scorer.RougeScorer(rouge_scores, use_stemmer=True)
+    
+    # Compute BERTScore
+    _, _, f1_scores = score(candidates, references, lang="en", verbose=False)
+    all_bert_f1 = [ f1.item() for f1 in f1_scores ]
+    
+    # Evaluate metrics
+    all_rouge1_f1, all_rougeL_f1, all_sts, all_bert_f1 = [], [], [], []
+    low_score_indices = []
 
-    for dataset_name,dataset_samples in all_samples_dict.items():
-        print(f">>> Loading values for {dataset_name}...")
-        for sample in dataset_samples:
-            if dataset_name == "alpaca" or (dataset_name == "openplatypus"):
-                prompt = make_alpaca_platypus_prompt(sample)
-            elif dataset_name == "tulu":
-                prompt = make_tulu_prompt(sample)
-            else:
-                print("Using default prompt")
-                prompt = make_default_prompt(sample)
-            all_prompts.append(prompt)
+    for i, (ref, cand) in enumerate(zip(references, candidates)):
+        emb_ref = sts_model.encode(ref, convert_to_tensor=True)
+        emb_cand = sts_model.encode(cand, convert_to_tensor=True)
+        raw_sts = util.cos_sim(emb_cand, emb_ref).item()
+        sts = (raw_sts + 1) / 2  # Normalize to [0, 1]
+        all_sts.append(sts)
+    
+        rouge_scores = rouge.score(ref, cand)
+
+        rouge1 = rouge_scores["rouge1"].fmeasure
+        rougeL = rouge_scores["rougeL"].fmeasure
+        all_rouge1_f1.append(rouge1)
+        all_rougeL_f1.append(rougeL)
+    
+
+    # Compute averages
+    n = len(references)
+    avg_bert = sum(all_bert_f1) / n
+    avg_rouge1 = sum(all_rouge1_f1) / n
+    avg_rougeL = sum(all_rougeL_f1) / n
+    avg_sts = sum(all_sts) / n
+    return avg_bert, avg_rouge1, avg_rougeL, avg_sts
 
 
-    print(">>> Loading tokenizer...")
-    tokenizer = AutoTokenizer.from_pretrained(reference_model_project_name, trust_remote_code= trust_remote_code)
 
-    from huggingface_hub import snapshot_download
-    snapshot_download(repo_id=reference_model_project_name)
-
-    print(">>> Initializing vLLM...")
-    llm = LLM(
-        model=reference_model_project_name,
-        #dtype=rouge_scores.get("dtype", "auto"),
-        #trust_remote_code=trust_remote_code,
-        tensor_parallel_size=device_count(),
-        #enforce_eager=rouge_scores.get("enforce_eager", True),
-        #enable_chunked_prefill=rouge_scores.get("enable_chunked_prefill", True),
-        #candidate_model_project_name=candidate_model_project_name
-    )
-
-    print("Completed the model initialization ")
-
-    sampling_params = SamplingParams(
-        temperature=rouge_scores.get("temperature", 0.0),
-        max_tokens=reference_model_task_name,
-        stop=["### Instruction:", "### Input:", "### Response:"],
-    )
-    print("Define sampling parameters")
-
-    print(">>> Running vLLM generation...")
-    outputs = llm.generate(all_prompts, sampling_params)
-
-    return all_prompts, outputs
 
 def main(configurations=None, args=None):
     if clearml_available:
@@ -94,56 +79,53 @@ def main(configurations=None, args=None):
     clearml_model = parse_argument(args["clearml_model"], bool)
     force_download = parse_argument(args["force_download"], bool)
     trust_remote_code = parse_argument(args["trust_remote_code"], bool)
-    reference_model_project_name = parse_argument(args["reference_model_project_name"], str)
-    candidate_model_project_name = parse_argument(args["candidate_model_project_name"], int)
-    candidate_model_task_name = parse_argument(args["candidate_model_task_name"], int)
-    reference_model_task_name = parse_argument(args["reference_model_task_name"], int)
     sts_model_id = args.get("sts_model_id", str)
     rouge_scores= args.get("rouge_scores", list)
-    save_directory = parse_argument(args["save_directory"], str)
+    #save_directory = parse_argument(args["save_directory"], str)
     tags = args.get("tags", None)
 
-
     print(args)
-
-    """
-
     if clearml_available:
+        reference_model_project_name = parse_argument(args["reference_model_project_name"], str)
+        candidate_model_project_name = parse_argument(args["candidate_model_project_name"], str)
+        candidate_model_task_name = parse_argument(args["candidate_model_task_name"], str)
+        reference_model_task_name = parse_argument(args["reference_model_task_name"], str)
         reference_task = Task.query_tasks(project_name=reference_model_project_name,task_name= reference_model_task_name, task_filter={'order_by': ['-last_update'], 'status': ['completed'] })
         reference_task = Task.get_task(reference_task[0])
-        reference_artifact_obj = reference_task.artifacts['jsonl model'].get_local_copy()
+        reference_file = reference_task.artifacts['jsonl model'].get_local_copy()
 
         candidate_task = Task.query_tasks(project_name=candidate_model_project_name,task_name= candidate_model_task_name, task_filter={'order_by': ['-last_update'], 'status': ['completed'] })
         candidate_task = Task.get_task(candidate_task[0])
-        candidate_artifact_obj = candidate_task.artifacts['jsonl model'].get_local_copy()
-
+        candidate_file = candidate_task.artifacts['jsonl model'].get_local_copy()
+        # add task query to get jsonl
     else:
-        reference_artifact_obj = None
-        candidate_artifact_obj = None
-
-    all_prompts, outputs = semantic_similarity_score_main(
+        ref_model_json = "Qwen_Qwen3-0.6B.jsonl"
+        cand_model_json = "RedHatAI_Qwen3-0.6B-quantized.w4a16.jsonl"
+        reference_file = os.path.join(SCORING_DIR, ref_model_json)
+        candidate_file = os.path.join(SCORING_DIR, cand_model_json)
+    
+    avg_bert, avg_rouge1, avg_rougeL, avg_sts = semantic_similarity_score_main(
+        reference_file,
+        candidate_file,
         sts_model_id,
         rouge_scores,
-        trust_remote_code,
-        save_directory,
     )
+    # Print summary
+    print("\n=== Averages (for Google Sheets) ===")
+    print("BERTScore F1 | ROUGE-1 F1 | ROUGE-L F1 | STS CosSim")
+    print(f"{avg_bert:.3f} | {avg_rouge1:.3f} | {avg_rougeL:.3f} | {avg_sts:.3f}")
 
-    OUTPUT_FILE = os.path.join(OUTPUT_DIR,f"{reference_model_project_name.replace('/', '_')}.jsonl")
-    print(">>> Writing outputs to file...")
-    with open(OUTPUT_FILE, "w") as fout:
-        for idx, (prompt, output) in enumerate(zip(all_prompts, outputs)):
-            response = output.outputs[0].text.strip()
-            fout.write(json.dumps({
-                "index": idx,
-                "prompt": prompt,
-                "response": response
-            }) + "\n")
-
-    print(f">>> Completed. Saved {len(outputs)} outputs to {OUTPUT_FILE}")
-
+    out_filename = f"scores_{ref_model_json.lower()}__vs__{cand_model_json.lower()}.txt"
+    out_filename = os.path.join(SCORING_DIR,out_filename)
+    # Save results
+    with open(out_filename, "w") as f_out:
+        f_out.write("BERTScore F1 | ROUGE-1 F1 | ROUGE-L F1 | STS CosSim\n")
+        f_out.write(f"{avg_bert:.3f} | {avg_rouge1:.3f} | {avg_rougeL:.3f} | {avg_sts:.3f}\n\n")
+    
+    print(f"\nSaved results to {out_filename}")
     if clearml_available:
-        task.upload_artifact("jsonl_output", OUTPUT_FILE)
-    """
+        task.upload_artifact("scores", out_filename)
+        print("Pushing clearml artifact")
 
 if __name__ == '__main__':
     main()
