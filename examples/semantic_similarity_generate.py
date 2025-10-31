0da978ee1b019fb5ee1a19bc7262005ee7ea9bad
chibu <>
2025-10-31 15:47:18 +0000

    update examples

--- examples/semantic_similarity_generate.py
+++ examples/semantic_similarity_generate.py
@@ -2,15 +2,16 @@ from automation.tasks import SemanticSimilarityGenerateTask
 
 task = SemanticSimilarityGenerateTask(
     project_name="semantic_similarity_debug",
-    task_name="semantic_generation_qwen3_14b_w4a16",
+    task_name="semantic_generation_qwen3_14b_w4a16_feedback",
+    #task_name="semantic_generation_qwen3_14b_feedback",
     branch="semantic_similarity",
     packages = ["huggingface-hub==0.34.3", "triton==3.3.1", "vllm==0.10.1.1"],
-    dataset_args = {"alpaca": "tatsu-lab/alpaca", "openplatypus": "garage-bAInd/Open-Platypus", "tulu": "allenai/tulu-3-sft-mixture"},
-    model_id="RedHatAI/Qwen3-14B-quantized.w4a16",
-    num_samples_per_dataset=330,
+    dataset_args = {"tatsu-lab/alpaca" : 300 , "garage-bAInd/Open-Platypus": "310", "allenai/tulu-3-sft-mixture": 320},
+    model_id="Qwen/Qwen3-14B",
     max_new_tokens=1024,
     max_model_len=4096,
     semantic_similarity_args={"enable-chunked-prefill": True, "enforce_eager": True, "dtype" :"auto", "device_map": "auto", "temperature": 0.0},
 )
 
 task.execute_remotely("oneshot-a100x1")
+
--- examples/semantic_similarity_score.py
+++ examples/semantic_similarity_score.py
@@ -2,16 +2,19 @@ from automation.tasks import SemanticSimilarityScoreTask
 
 task = SemanticSimilarityScoreTask(
     project_name="semantic_similarity_debug",
-    task_name="semantic_scoring_14b",
+    #task_name="semantic_scoring_14b",
+    task_name="semantic_scoring_4b",
     branch="semantic_similarity",
     packages = ["huggingface-hub==0.34.3", "networkx==3.4.2", "datasets==4.2.0", "rouge_score==0.1.2", "bert-score==0.3.13", "sentence-transformers==5.1.1", "matplotlib"],
     reference_model_project_name="semantic_similarity_debug",
     candidate_model_project_name="semantic_similarity_debug",
-    reference_model_task_name="semantic_generation_qwen3_14b_base",
-    candidate_model_task_name="semantic_generation_qwen3_14b_w4a16",
+    reference_model_task_name="semantic_generation_qwen3_14b_feedback",
+    #reference_model_task_name="semantic_generation_qwen3_14b_base",
+    candidate_model_task_name="semantic_generation_qwen3_14b_w4a16_feedback",
+    #candidate_model_task_name="semantic_generation_qwen3_14b_w4a16",
     sts_model_id="all-MiniLM-L6-v2",
     rouge_scores=["rouge1", "rougeL"],
-    scoring_args={"f1": 0.75, "rouge1": 0.5, "sts": 0.75},
+    low_score_threshold_args={"f1": 0.79, "rouge1": 0.65, "sts": 0.71},
 )
 
 task.execute_remotely("oneshot-a100x1")
