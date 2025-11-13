3c400ecaa8c997ae113f64ef704c36fe3a3e968c
chibu <>
2025-11-13 10:58:55 +0000

    snapshot download only

--- src/automation/tasks/scripts/semantic_similarity_generate_script.py
+++ src/automation/tasks/scripts/semantic_similarity_generate_script.py
@@ -70,14 +70,12 @@ def semantic_similarity_generate_main(
     if clearml_model:
         HUGGINGFACE_DIR = Model(model_id).get_local_copy()
     else:
-        print(">>> Downloading snapshot ...")
-        if "mistral" not in model_id :
-            snapshot_download(repo_id=model_id, local_dir=HUGGINGFACE_DIR)
+        snapshot_download(repo_id=model_id, local_dir=HUGGINGFACE_DIR)
     
     try:
         print(f"Initializing vLLM: {model_id}...")
         llm = LLM(
-            model=model_id if "mistral" in model_id else HUGGINGFACE_DIR,
+            model=HUGGINGFACE_DIR,
             dtype=semantic_similarity_args.get("dtype", "auto"),
             trust_remote_code=trust_remote_code,
             tensor_parallel_size=device_count(),
