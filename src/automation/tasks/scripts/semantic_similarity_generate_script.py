95d924a70631b1b5f275c18517c2f2101360b521
chibu <>
2025-11-14 19:07:36 +0000

    remove formatting

--- src/automation/tasks/scripts/semantic_similarity_generate_script.py
+++ src/automation/tasks/scripts/semantic_similarity_generate_script.py
@@ -81,23 +81,22 @@ def semantic_similarity_generate_main(
             print("Model weights not found")
 
         print("Download snapshot")
-        HUGGINGFACE_DIR = "/home"
-        snapshot_download(repo_id=model_id, local_dir=HUGGINGFACE_DIR)
-        print(os.listdir(HUGGINGFACE_DIR))
+        #HUGGINGFACE_DIR = "/home"
+        #snapshot_download(repo_id=model_id, local_dir=HUGGINGFACE_DIR)
+        """
         if "mistral" in model_id.lower() and "quantized" in model_id.lower():
-            #os.makedirs("/tmp", exist_ok=True)
             from huggingface_hub import hf_hub_download
             hf_hub_download(repo_id="mistralai/Mistral-Small-3.1-24B-Instruct-2503", filename="params.json", local_dir=HUGGINGFACE_DIR)
-            #print(os.listdir("/tmp"))
             #import shutil
             #shutil.copy("/tmp/params.json", HUGGINGFACE_DIR)
+        """
         print(os.listdir(HUGGINGFACE_DIR))
     
     try:
         print(f"Initializing vLLM: {model_id}...")
         llm = LLM(
-            model= HUGGINGFACE_DIR,
-            #model= model_id,
+            #model= HUGGINGFACE_DIR,
+            model= model_id,
             dtype=semantic_similarity_args.get("dtype", "auto"),
             trust_remote_code=trust_remote_code,
             tensor_parallel_size=device_count(),
