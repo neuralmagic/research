2884ed5b3c031dc024a9aabed8f6d21a498d5bc7
chibu <>
2025-11-19 17:42:28 +0000

    clean tulu

--- src/automation/datasets/tulu.py
+++ src/automation/datasets/tulu.py
@@ -2,19 +2,3 @@
 def make_tulu_prompt(sample):
     messages = sample["messages"][:-1]
     return messages
-
-"""
-def make_tulu_prompt(sample):
-    messages = []
-    for m in sample["messages"]:
-        role = m.get("role", "")
-        content = m.get("content", "").strip()
-        if role == "user":
-            convo = {
-                "role": role,
-                "content": content,
-            }
-            messages.append(convo)
-
-    return messages
-"""
