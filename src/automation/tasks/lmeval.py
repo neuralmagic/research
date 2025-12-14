6c41828612abd356f637e5e164fc56f1117bfc3d
chibu <>
2025-12-14 18:44:22 +0000

    update triton version

--- src/automation/tasks/lmeval.py
+++ src/automation/tasks/lmeval.py
@@ -38,7 +38,7 @@ class LMEvalTask(BaseTask):
         "datasets==4.4.1",
         "emoji==2.15.0",
         "huggingface-hub==0.36.0",
-        "triton==3.3.1",
+        "triton==3.5.0",
         "networkx==3.4.2",
         "hf-xet==1.2.0",
         "immutabledict==4.2.2",
